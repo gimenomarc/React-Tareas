a6a70eceec2a34e6213bc638ea66371220d18844
SirCrewete <gimenocrew1996@gmail.com>
2022-05-25 20:20:51 +0200

    Tareas component ok

--- tareas/src/App.js
+++ tareas/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ListaDeTareas from './Componentes/ListaDeTareas';
+import Tarea from './Componentes/Tarea';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
         <div className="container_tareas">
           <h1>Aplicación para la creación de tareas</h1>
           <ListaDeTareas />
+          <Tarea
+          texto="Aprendiendo React" />
         </div>
     </div>
   );
--- tareas/src/Componentes/Tarea.jsx
+++ tareas/src/Componentes/Tarea.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../Styles/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tarea () {
-    <>
-    
-    
-    
-    </>
+function Tarea ({ id, texto, completada, completarTarea, eliminarTarea }) {
+    return (
+        <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
+            <div
+            className='tarea-texto'>
+                {texto}
+            </div>
+            <div className='tarea-contenedor-iconos'>
+                <AiOutlineCloseCircle className='tarea-icono'/>
+            </div>
+        </div>
+        
+    )
 }
 
 export default Tarea;
